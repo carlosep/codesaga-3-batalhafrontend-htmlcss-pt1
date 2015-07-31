999cc23ca797852c54834185c9c06f4720252e01
Alan Rafael R. Batista <alanrrb@gmail.com>
2015-07-30 21:27:04 -0300

    Add util.js

--- js/challenge.js
+++ js/challenge.js
@@ -1,33 +1,14 @@
 $(document).ready(function() {
   $('#solution').load(function() {
-    var title = $("#solution").contents().find("title").text();
-    var h1 = $("#solution").contents().find("h1").text();
-    var p = $("#solution").contents().find("p").text();
+    var title = getText("title");
+    var h1 = getText("h1");
+    var p = getText("p");
     var errors = new Array();
 
     verify(strcmp("Meu Currículo", title), "The tag \'title\' is missing or wrong.", errors);
     verify(h1.length == 0, "The tag 'h1' is missing or empty.", errors);
     verify(p.length == 0, "You must write at least one paragraph.", errors);
 
-    var result = $("#result");
-    if (errors.length == 0){
-      result.addClass("alert-success");
-      result.append("Success. You are ready to the next challenge.");
-    }
-    else {
-      result.addClass("alert-danger");
-      result.append("Ops... You still have " + errors.length + " errors:<br>");
-      result.append(errors.join("<br>"));
-    }
+    showMessage(errors);
   });
 });
-
-function verify(expr, message, errors) {
-  if(expr) {
-    errors.push(message);
-  }
-}
-
-function strcmp(a, b) {
-  return (a<b?-1:(a>b?1:0));
-}
